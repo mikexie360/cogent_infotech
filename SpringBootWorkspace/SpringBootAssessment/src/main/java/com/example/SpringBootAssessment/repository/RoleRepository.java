58fecc0130ba937c0e6825281c4634b2800c7cf0
mikexie360 <mxie360@gmail.com>
2023-04-14 14:16:43 -0500

    Spring boot Assessment

--- SpringBootWorkspace/SpringBootAssessment/src/main/java/com/example/SpringBootAssessment/repository/RoleRepository.java
+++
@@ -1,8 +0,0 @@
-package com.example.SpringBootAssessment.repository;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-import com.example.SpringBootAssessment.entity.*;
-
-public interface RoleRepository extends JpaRepository<Role,Integer>{
-
-}
